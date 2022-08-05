fb37a1dcf4ff32e9c80b3c0b07f5b0f0c5384cd5
Louise Rodrigues <nutrilouiserodrigues@gmail.com>
2022-08-05 17:41:11 -0300

    initial commit

---
+++ Enumeracoes/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Enumeracoes.Entities.Enums
+{
+    public enum OrderStatus: int
+    {
+        PendingPayment = 8,
+        Processing = 1,
+        Delivered = 3
+    }
+}
---
+++ Enumeracoes/Program.cs
@@ -0,0 +1,28 @@
+﻿using Enumeracoes.Entities;
+using Enumeracoes.Entities.Enums;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            var order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os;
+            Enum.TryParse("Delivered",out os);
+            
+            Console.WriteLine(os); 
+            Console.WriteLine(txt); 
+        }
+    }
+}
